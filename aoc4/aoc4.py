9e1f2958b247e75e765834ace11741f64449440f
Robert Nolet <r.w.nolet@hva.nl>
2023-12-04 17:03:30 +0100

    aoc4 solution

--- aoc4/aoc4.py
+++ aoc4/aoc4.py
@@ -9,18 +9,13 @@ Created on Mon Dec  4 15:50:31 2023
 def score(n):
     return 0 if n == 0 else 2**(n-1)
 
-
-counts = []
-for line in open('input.txt'):
-    p1, p2 = line.split(':')    
-    p2, p3 = p2.split('|')
-    counts.append(len(set(map(int, p2.split())) & set(map(int, p3.split()))))
-n = len(counts)
+data = [(set(map(int, p.split())) for p in line.split(':')[1].split('|')) for line in open('input.txt')]
+counts = [len(x&y) for x,y in data]
 
 print('Part 1:', sum(map(score, counts)))
 
-cards = [1]*n
-for c in range(n):
+cards = [1]*len(data)
+for c in range(len(data)):
     for i in range(counts[c]):
         cards[c+i+1]+=cards[c]
 
